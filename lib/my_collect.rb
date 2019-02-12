6b16fc7102ff657423e048125863e23ef45384cc
Theodore Kimana <kimana.theodore@gmail.com>
2019-02-12 16:13:11 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,16 @@
 
-
+def my_collect(empty_array)
+empty_array = []
+    counter = 0
+end
+def my_collect(array)
+  array.upcase = []
+  counter = 1
+end
+def my_collect(languages)
+language.upcase
+language.upcase =  ["ruby", "javascript","python","objective-c"]
+end
+def my_collect(students)
+  ["Tim","Tom","Sophie","Antoin"]
+end
