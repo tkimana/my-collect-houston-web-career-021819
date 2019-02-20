cab54f8269bc94aa11920b47b92146a3f44b14b7
Theodore Kimana <kimana.theodore@gmail.com>
2019-02-20 14:56:47 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -8,9 +8,11 @@ def my_collect(array)
   counter = 1
 end
 def my_collect(languages)
-language.upcase
 language.upcase =  ["RUBY", "JAVASCRIPT","PYTHON","OBJECTIVE-C"]
+my_collect(language_upcase).each do |language|
+  
 end
 def my_collect(students)
-  ["Tim","Tom","Sophie","Antoin"]
+  array = ["Tim","Tom","Sophie","Antoin"]
+  my_collect(students).each
 end
